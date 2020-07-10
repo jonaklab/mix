d069ba0fe779f4edd640913cd5e247be600b1c0f
Lenin Hasda <leninhasda@gmail.com>
2020-07-10 10:33:39 +0200

    cleanup code refactor search results

--- src/App.vue
+++ src/App.vue
@@ -1,76 +1,25 @@
 <template>
   <div class="container">
     <AppHeader />
+    
     <section class="section">
-      <h1 class="title">Section</h1>
-      <h2 class="subtitle">
-        A simple container to divide your page into <strong>sections</strong>, like the one you're currently reading
-      </h2>
-      <div class="field has-addons">
+      <div class="field has-addons has-addons-fullwidth">
         <div class="control">
-          <input v-model="searchedInput" class="input" type="text" placeholder="Search on youtube">
+            <input v-model="searchedInput" class="input is-large" type="text" placeholder="Search on youtube">
         </div>
         <div class="control">
-          <button @click="search" class="button is-info">
-            Search
-          </button>
+            <button @click="search" class="button is-info is-large">Search</button>
         </div>
       </div>
     </section>
-  </div>
-  <!-- ----------------------------------------------------------------- -->
-  <!-- <div id="app">
-    <div class="container-fluid">
-      <a href="#" class="list-group-item list-group-item-action" v-for='dit in selitems'>
-        <div class="card">
-          <div class="row no-gutters">
-            <div class="col-md-4">
-              <img :src="dit.snippet.thumbnails['default'].url" class="card-img" alt="...">
-            </div>
-            <div class="col-md-8">
-              <div class="card-body">
-                <h5 class="card-title">{{dit.snippet.title}}</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </a>
 
-      <div>
-        <form>
-          <input class="form-control form-control-lg" type="text" placeholder="search" v-model="q">
-          <button type="button" v-on:click="searchMe" class="btn btn-primary">Submit</button>
-        </form>
-      </div>
-      <a href="#" class="list-group-item list-group-item-action" v-for='dit in items'>
-        <div class="card">
-          <div class="row no-gutters">
-            <div class="col-md-4">
-              <img :src="dit.snippet.thumbnails['default'].url" class="card-img" alt="...">
-            </div>
-            <div class="col-md-8">
-              <div class="card-body">
-                <h5 class="card-title">{{dit.snippet.title}}</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
-                <button type="button" v-on:click="add(dit)" class="btn btn-success">Add</button>
-                </p>
-                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </a>
-    </div>
-  </div> -->
+    <SearchResults :searchResults="searchResults" />
+  </div>
 </template>
 
 <script>
 import AppHeader from './components/AppHeader'
-// import Sidebar from './components/Sidebar.vue'
-// import ListArea from './components/ListArea.vue'
-// import DetailsArea from './components/DetailsArea.vue'
+import SearchResults from './components/SearchResults'
 
 import axios from 'axios'
 import config from '../config.json'
@@ -80,28 +29,26 @@ export default {
   data () {
     return {
       searchedInput: null,
-      items: [],
+      searchResults: [],
       selitems: []
     }
   },
   components: {
-    AppHeader
-  //  Sidebar,
-  //  ListArea,
-  //  DetailsArea
+    AppHeader,
+    SearchResults
   },
   methods: {
     add(item) {
       this.selitems.push(item)
     },
     search() {
-      let url= `https://www.googleapis.com/youtube/v3/search?q=${this.q}&part=snippet&key=${config.api_key}`
+      let url= `https://www.googleapis.com/youtube/v3/search?q=${this.searchedInput}&part=snippet&key=${config.api_key}`
       axios.get(url).then(res => {
-        console.log(res)
-        this.items = res.data.items
+        window.console.log(res)
+        this.searchResults = res.data.items
       })
       .catch( err => {
-        console.log(err)
+        window.console.log(err)
       })
     }
   }
